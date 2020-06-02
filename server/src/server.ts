e470a90eabc247cbf19fbd45bb92982960d2707d
Silvio Melo Filho <smfilho@gmail.com>
2020-06-02 13:04:52 -0500

    initial server and web files

---
+++ server/src/server.ts
@@ -0,0 +1,42 @@
+import express, { response, request } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const users = [
+  'Cu',
+  'Azul',
+  'Olar',
+  'Praisteichon',
+  'cu de foca'
+];
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+  return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+  
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
+});
+
+app.listen(3333);
