3c92afc09f6f2fa31b72bccbfe522c4a6ad81ad5
Silvio Melo Filho <smfilho@gmail.com>
2020-06-08 20:39:56 -0500

    Routes updated

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,6 +8,7 @@ routes.get('/items', async (request, response) => {
 
   const serializedItems = items.map(item => {
     return{
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
@@ -15,6 +16,47 @@ routes.get('/items', async (request, response) => {
 
 
   return response.json(serializedItems);
+});
+
+routes.post('/points', async(request, response) => {
+  const {
+    name,
+    email,
+    phone,
+    latitude,
+    longitude,
+    city,
+    state,
+    zip,
+    items
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    phone,
+    latitude,
+    longitude,
+    city,
+    state,
+    zip
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ sucess: true });
 });
 
 export default routes;
